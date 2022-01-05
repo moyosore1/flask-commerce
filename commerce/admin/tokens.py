4a0c3a71c36f61b251d4adb90f2262c8076d642b
moyosore1 <35351716+moyosore1@users.noreply.github.com>
2022-01-05 17:08:27 -0500

    Update tokens.py

--- commerce/admin/tokens.py
+++ commerce/admin/tokens.py
@@ -5,7 +5,7 @@ from commerce import db
 
 
 from .routes import admin
-from .auth import basic_auth
+from .auth import basic_auth, token_auth
 
 
 @admin.route('/tokens', methods=['POST'])
@@ -14,3 +14,11 @@ def get_token():
     token = basic_auth.current_user().get_token()
     db.session.commit()
     return jsonify({'token': token})
+
+
+@admin.route('/tokens', methods=['DELETE'])
+@token_auth.login_required
+def revoke_token():
+    token_auth.current_user().revoke_token()
+    db.session.commit()
+    return '', 204
