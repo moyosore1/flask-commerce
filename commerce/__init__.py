10d2beafa21c7a49628dad2899ce9aba01fc63a3
Kahmyl <59609015+Kahmyl@users.noreply.github.com>
2022-01-09 13:51:09 +0100

    Authentication

--- commerce/__init__.py
+++ commerce/__init__.py
@@ -1,23 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
-
-from .config import Config
 
 db = SQLAlchemy()
-
 
 bcrypt = Bcrypt()
 
 
-def create_app(config_class=Config):
+def create_app(config=None):
     app = Flask(__name__)
-    app.config.from_object(Config)
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+    if app.config['ENV'] == 'production':
+        app.config.from_object('config.Production')
+    
+    else:
+        app.config.from_object('config.Development')
+
     db.init_app(app)
 
     bcrypt.init_app(app)
+    
+    CORS(app, supports_credentials=True)
 
     from commerce.store.routes import store
     from commerce.users.routes import users
@@ -25,7 +29,7 @@ def create_app(config_class=Config):
     app.register_blueprint(users)
     app.register_blueprint(store)
 
-    # from cbtexam.admin.models import BlacklistToken
+    from commerce.users.models import Users
     # with app.app_context():
     #     db.create_all()
 
---
+++ commerce/users/models.py
@@ -0,0 +1,17 @@
+from commerce import  db
+
+class Users(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return f"User('{self.name}', '{self.email}')"
