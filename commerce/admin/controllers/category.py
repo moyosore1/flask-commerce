c49c9e0060673f83f34ae2ab32001ebbf26fb681
moyosore1 <moyosoreolumideobi@gmail.com>
2022-04-30 04:43:57 -0400

    update category edit

--- commerce/admin/controllers/category.py
+++ commerce/admin/controllers/category.py
@@ -1,17 +1,19 @@
-import os
-
-from unicodedata import category
 from flask import Blueprint, jsonify, request
 from werkzeug.http import HTTP_STATUS_CODES
+from slugify import slugify
+
 
 from commerce import db
 from commerce.errors import bad_request
-from commerce.store.models import  Category
+from commerce.store.models import Category
 
 
 def api_category():
-    category = Category.query.all()
+    category = Category.query.order_by(Category.id).all()
+    print(len(category))
+
     return jsonify([*map(category_serializer, category)])
+
 
 def api_create_category():
     if request.method == "POST":
@@ -30,22 +32,33 @@ def api_create_category():
             result = add_category(data['name'])
             return result, HTTP_STATUS_CODES.get(201)
 
+
 def api_edit_category(id):
     if request.method == "PUT":
-        category = Category.query.get_or_404(id)
-        data = request.json() or request.form
+        categoryObj = Category.query.get_or_404(id)
+        data = request.get_json() or request.form
         if 'name' in data:
-            category.name = data.get("name")
+            new_category_name = data.get("name")
+            new_category_slug = slugify(new_category_name)
+            category = Category.query.filter_by(
+                name=new_category_name, slug=new_category_slug).first()
+            if category:
+                return "Category already exists", HTTP_STATUS_CODES.get(400)
+
+            categoryObj.name = new_category_name
+            categoryObj.slug = new_category_slug
             db.session.commit()
-            return jsonify(category_serializer(category)), HTTP_STATUS_CODES.get(200)
+            return jsonify(category_serializer(categoryObj)), HTTP_STATUS_CODES.get(200)
+
 
 def api_delete_category(id):
     if request.method == "DELETE":
-        category = Category.query.get_or_404(id) 
-        name = category.name   
+        category = Category.query.get_or_404(id)
+        name = category.name
         db.session.delete(category)
         db.session.commit()
         return jsonify({"Success": f'Successfully deleted product {name}'}), HTTP_STATUS_CODES.get(204)
+
 
 def category_serializer(category):
     return{
